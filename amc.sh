0ebd9ede4eeb9932314f108441ca33f2916fba0e
Matthew Jorgensen <matthew@jrgnsn.net>
2021-01-03 17:11:57 -0600

    Use central excludes file

--- amc.sh
+++ amc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -xu
 filebot -script fn:amc --output "/mnt/Media" --action copy --conflict skip \
     --log-file amc.log -non-strict \
-    --def excludeList=".excludes" unsorted=y music=y artwork=y "$@"
+    --def excludeList="/home/mjorgensen/amc.excludes" unsorted=y music=y artwork=y "$@"
