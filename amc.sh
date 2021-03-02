f32159ecde43817d84c75946200607527b7d5bff
Matthew Jorgensen <matthew@jrgnsn.net>
2021-03-02 17:18:09 -0600

    Use .env file to store path to media

--- amc.sh
+++ amc.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -xu
-filebot -script fn:amc --output "/mnt/Media" --action copy --conflict skip \
+
+source .env
+
+filebot -script fn:amc --output "$PLEX_MEDIA_DIR" --action copy --conflict skip \
     --log-file amc.log -non-strict \
     --def excludeList="/home/mjorgensen/amc.excludes" unsorted=y music=y artwork=y "$@"
