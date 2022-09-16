3ddabb01d0bf61b31454844acbc85c2ce28fba7e
prplecake <me@prplecake.com>
2022-09-15 21:40:46 -0500

    update username

--- amc.sh
+++ amc.sh
@@ -5,4 +5,4 @@ source "$ROOTDIR/.env"
 
 filebot -script fn:amc --output "$PLEX_MEDIA_DIR" --action copy --conflict skip \
     --log-file amc.log -non-strict \
-    --def excludeList="/home/mjorgensen/amc.excludes" unsorted=y music=y artwork=y "$@"
+    --def excludeList="/home/prplecake/amc.excludes" unsorted=y music=y artwork=y "$@"
